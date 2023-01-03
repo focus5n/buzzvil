de1fe040cd57ecfa34d6576956cbc2c1fe503495
hans <focus5n@naver.com>
2023-01-03 23:42:45 +0900

    Initial commit

---
+++ src/main/java/com/buzzvil/rewardserver/RewardServerApplication.java
@@ -0,0 +1,13 @@
+package com.buzzvil.rewardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buzzvil/rewardserver/RewardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzzvil.rewardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
